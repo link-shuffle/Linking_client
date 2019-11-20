637e699db36b6f2604a81d30ad00c186e47bbbbd
snowjang24 <asdka4618@gmail.com>
2019-11-20 23:14:20 +0900

    fix: commit error missing save

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,12 +1,29 @@
 import React from "react";
-import Login from "../../pages/Login.jsx";
-import Main from "../../pages/Main.jsx";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+import AuthRoutes from "./AuthRoutes";
+import Main from "../../pages/Main";
+import Login from "../../pages/Login";
+import Search from "../../pages/Search";
+import NotFound from "../../pages/NotFound";
+
+const authenticated = true;
 function App() {
   return (
     <div className="App">
-      <Login />
-      <Main />
+      <Router>
+        <Switch>
+          <AuthRoutes
+            exact
+            path="/"
+            authenticated={authenticated}
+            component={Main}
+          />
+          <Route path="/login" component={Login} />
+          <Route path="/Search" component={Search} />
+          <Route component={NotFound} />
+        </Switch>
+      </Router>
     </div>
   );
 }
