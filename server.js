39856b1105ab16e7da56cac47f0d35698f58f052
doyuni <gdypc@naver.com>
2019-12-06 05:03:57 +0900

    [Server] client 서버 설정

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/build'));
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Listening on port ${port}...`));
