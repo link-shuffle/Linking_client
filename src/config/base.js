382723a3c75ee9caeb16234b9e815e9fc2b2256e
snowjang24 <asdka4618@gmail.com>
2019-12-08 22:02:29 +0900

    fix: fix api href add port Numb

--- src/config/base.js
+++ src/config/base.js
@@ -1,4 +1,4 @@
-// http://106.10.39.188
+// http://106.10.39.188:1024
 // http://localhost:1024
 
-export const baseUrl = "http://106.10.39.188";
+export const baseUrl = "http://106.10.39.188:1024";
